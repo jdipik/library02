b7aeff9fb1d22c76d26007f15ba4173ebfb440a8
jdipik <jdipik@gmail.com>
2018-07-18 12:58:43 +0000

    Test commit 2

--- library02-BU-05/src/main/java/com/rjr/library02/ServletInitializer.java
+++ library02-BU-05/src/main/java/com/rjr/library02/ServletInitializer.java
@@ -11,5 +11,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(Library02Application.class);
 	}
+	
+	//	AAAAA
 
 }
